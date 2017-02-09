65936767040afd3edc4b321c4d2cd0bf04625d37
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2017-02-09 21:05:59 +0100

    Add a test

--- server/registry/generate-files.test.ts
+++ server/registry/generate-files.test.ts
@@ -23,7 +23,6 @@ it('creates the right structure', async () => {
     }
   };
 
-  // const memoryFs = new fs();
   await generateFiles(version);
 
   expect(fs.readdirSync(`${TEMP_ROOT}/anonymous/test/1.0.0`)).toEqual(['directory']);
@@ -54,7 +53,6 @@ it('handles multiple files', async () => {
     }
   };
 
-  // const memoryFs = new fs();
   await generateFiles(version);
 
   expect(fs.readdirSync(`${TEMP_ROOT}/anonymous/test/2.0.0`)).toEqual(['directory']);
@@ -90,7 +88,6 @@ it('handles nested files', async () => {
     }
   };
 
-  // const memoryFs = new fs();
   await generateFiles(version);
 
   expect(fs.readdirSync(`${TEMP_ROOT}/anonymous/test/3.0.0`)).toEqual(['directory']);
@@ -130,12 +127,55 @@ it('handles multiple root directories', async () => {
     }
   };
 
-  // const memoryFs = new fs();
   await generateFiles(version);
 
   expect(fs.readdirSync(`${TEMP_ROOT}/anonymous/test/4.0.0`)).toEqual(['directory', 'directoryalso']);
   expect(fs.readFileSync(`${TEMP_ROOT}/anonymous/test/4.0.0/directory/firstmodule.js`).toString()).toEqual('hello world');
   expect(fs.readFileSync(`${TEMP_ROOT}/anonymous/test/4.0.0/directoryalso/aaaaaa.js`).toString()).toEqual('hello world2');
+});
+
+it('handles multiple child directories', async () => {
+  const version = {
+    version: '4.0.0',
+    sandbox: {
+      slug: 'test'
+    },
+    source: {
+      directories: [{
+        id: 1,
+        directory_id: null,
+        title: 'directory'
+      }, {
+        id: 3,
+        directory_id: 1,
+        title: 'directoryalso'
+      }, {
+        id: 2,
+        directory_id: 1,
+        title: 'hello'
+      }],
+      modules: [{
+        directory_id: 3,
+        title: 'firstmodule.js',
+        code: 'hello world',
+      }, {
+        directory_id: 3,
+        title: 'aaaaaa.js',
+        code: 'hello world2',
+      }, {
+        directory_id: 2,
+        title: 'aaaaaa.js',
+        code: 'hello world3',
+      }]
+    }
+  };
+
+  await generateFiles(version);
+
+  expect(fs.readdirSync(`${TEMP_ROOT}/anonymous/test/4.0.0`)).toEqual(['directory']);
+  expect(fs.readFileSync(`${TEMP_ROOT}/anonymous/test/4.0.0/directory/directoryalso/firstmodule.js`).toString()).toEqual('hello world');
+  expect(fs.readFileSync(`${TEMP_ROOT}/anonymous/test/4.0.0/directory/directoryalso/aaaaaa.js`).toString()).toEqual('hello world2');
+  expect(fs.readFileSync(`${TEMP_ROOT}/anonymous/test/4.0.0/directory/hello/aaaaaa.js`).toString()).toEqual('hello world3');
 });
 
 afterAll(() => deleteDirectory(TEMP_ROOT))
